7426aee88e1324326866257a806028bf3449c3bc
Iulian <petruiulian01@gmail.com>
2021-07-11 16:51:47 +0300

    Add files via upload

--- deletemsgs/deletemsgs.py
+++ deletemsgs/deletemsgs.py
@@ -1,5 +1,5 @@
 from discord.channel import CategoryChannel
-from discord.ext.commands import MissingPermissions
+from discord.ext.commands import MissingPermissions, has_permissions
 from redbot.core import commands
 import discord
 
@@ -11,11 +11,11 @@ class DeleteMsgs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    trigger = False
 
     @commands.command()
     async def nuke(self, ctx, channel_name: discord.TextChannel):
-        """Deletes a channel mentioned by the server owner/Administrator Role."""
+        """Deletes a channel mentioned by the server/bot owner."""
         if ctx.message.author.guild_permissions.administrator:
             if channel_name is not None:
                 await channel_name.clone(reason="Has been nuked")
@@ -24,3 +24,22 @@ class DeleteMsgs(commands.Cog):
                 await ctx.send(f'No channel named **{channel_name}** was found')
         else:
             await ctx.send("You don't have permission to do that.")
+    @commands.command()
+    async def autodelete(self, ctx, value):
+        """Use on/off to enable or disable autodelete function."""
+        if value == "on":
+           global trigger
+           trigger = True
+           await ctx.send("You turned on the autodeletion.")
+        elif value == "off":
+            trigger = False
+            await ctx.send("You turned off the autodeletion.")
+        else:
+            await ctx.send("You gave a wrong value")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self,member):
+        global trigger
+        if trigger:
+            for c in member.guild.text_channels:
+                await c.purge(limit=None, check=lambda m: m.author==member)
